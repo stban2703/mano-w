9328b12fa99ab82e456628719bafb885ba2c76bf
stban2703 <s.t.ban1401@gmail.com>
2021-03-30 19:02:40 -0500

    Added topic list and messages list render

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,222 @@
 const chatbot = document.querySelector(".chatbot");
+const chatbotChat = document.querySelector(".chatbot__chat");
 const openChatbotBtn = document.querySelector(".chatbot__openBtn");
 const closeChatbotBtn = document.querySelector(".chatbot__closeBtn");
+
+const chatBotOptionsList = [
+    {
+        optionId: 0,
+        value: "Productos del banco",
+        imageUrl: "./src/images/chatbotproducts.svg",
+        itemList: [
+            {
+                itemId: 0,
+                value: "CDT",
+                imageUrl: "./src/images/chatbotproducts.svg",
+                itemList: [
+                    {
+                        itemId: 0,
+                        value: "Información",
+                        imageUrl: "./src/images/chatbotproducts.svg",
+                        redirectUrl: "productInformation.html"
+                    },
+                    {
+                        itemId: 1,
+                        value: "Duda específica",
+                        imageUrl: "./src/images/chatbotproducts.svg",
+                        isFinal: true
+                    }
+                ]
+            },
+            {
+                itemId: 1,
+                value: "Cuenta de ahorros",
+                imageUrl: "./src/images/chatbotproducts.svg",
+                itemList: [
+                    {
+                        itemId: 0,
+                        value: "Información",
+                        imageUrl: "./src/images/chatbotproducts.svg",
+                        redirectUrl: "productInformation.html"
+                    },
+                    {
+                        itemId: 1,
+                        value: "Duda específica",
+                        imageUrl: "./src/images/chatbotproducts.svg",
+                        isFinal: true
+                    }
+                ]
+            },
+            {
+                itemId: 2,
+                value: "Microcréditos",
+                imageUrl: "./src/images/chatbotproducts.svg",
+                itemList: [
+                    {
+                        itemId: 0,
+                        value: "Información",
+                        imageUrl: "./src/images/chatbotproducts.svg",
+                        redirectUrl: "productInformation.html"
+                    },
+                    {
+                        itemId: 1,
+                        value: "Duda específica",
+                        imageUrl: "./src/images/chatbotproducts.svg",
+                        isFinal: true
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        optionId: 1,
+        value: "Preguntas frecuentes",
+        imageUrl: "./src/images/chatbotquestions.svg",
+        redirectUrl: "frequentQuestions.html"
+    },
+    {
+        optionId: 2,
+        value: "Entrenamiento digital",
+        imageUrl: "./src/images/chatbotentertaiment.svg",
+        itemList: [
+            {
+                itemId: 0,
+                value: "Acceder a eventos",
+                imageUrl: "./src/images/chatbotproducts.svg",
+                itemList: [
+                    {
+                        itemId: 0,
+                        value: "Videos informativos",
+                        imageUrl: "./src/images/chatbotproducts.svg",
+                        redirectUrl: "productInformation.html"
+                    },
+                    {
+                        itemId: 1,
+                        value: "Duda específica",
+                        imageUrl: "./src/images/chatbotproducts.svg",
+                        isFinal: true
+                    }
+                ]
+            },
+            {
+                itemId: 1,
+                value: "Tablero de sueños",
+                imageUrl: "./src/images/chatbotproducts.svg",
+                itemList: [
+                    {
+                        itemId: 0,
+                        value: "Videos informativos",
+                        imageUrl: "./src/images/chatbotproducts.svg",
+                        redirectUrl: "productInformation.html"
+                    },
+                    {
+                        itemId: 1,
+                        value: "Duda específica",
+                        imageUrl: "./src/images/chatbotproducts.svg",
+                        isFinal: true
+                    }
+                ]
+            },
+            {
+                itemId: 2,
+                value: "Uso del chabot",
+                imageUrl: "./src/images/chatbotproducts.svg",
+                itemList: [
+                    {
+                        itemId: 0,
+                        value: "Videos informativos",
+                        imageUrl: "./src/images/chatbotproducts.svg",
+                        redirectUrl: "productInformation.html"
+                    },
+                    {
+                        itemId: 1,
+                        value: "Duda específica",
+                        imageUrl: "./src/images/chatbotproducts.svg",
+                        isFinal: true
+                    }
+                ]
+            }
+        ]
+    }
+]
+
+const messageList = [
+    {
+        id: 0,
+        text: "Dime en qué te puedo ayudar",
+        type: "asesor",
+    },
+    {
+        id: 1,
+        type: "bot",
+        title: "Elige el tema del que desees obtener información",
+        optionsList: chatBotOptionsList
+    }
+]
+
+//renderChatbotTopics(chatBotOptionsList);
+
+renderChatMessages(messageList);
+
+function renderChatMessages(list) {
+    list.forEach((elem) => {
+        const newMessage = document.createElement("div");
+        newMessage.classList.add("message");
+        switch (elem.type) {
+            case "bot":
+                newMessage.classList.add("message--topics");
+                const newTopicList = document.createElement("ul");
+                newTopicList.classList.add("message__topicsList");
+                newMessage.innerHTML = `
+                <p class="message__title">${elem.title}</p>
+                `
+                newMessage.appendChild(newTopicList);
+                renderChatbotTopics(newTopicList, elem.optionsList);
+                break;
+            case "user":
+                newMessage.classList.add("message--mine");
+                newMessage.innerHTML = `
+                    <p class="message__text">${elem.text}</p>
+                    <p class="message__hour">15:10</p>
+                `
+                break;
+            default:
+                newMessage.innerHTML = `
+                    <p class="message__text">${elem.text}</p>
+                    <p class="message__hour">15:10</p>
+                `
+                break;
+        }
+
+        chatbotChat.appendChild(newMessage);
+    })
+}
+
+function renderChatbotTopics(htmlElement, list) {
+    //const chatbotTopicList = document.querySelector(".message__topicsList");
+    list.forEach((elem) => {
+        const newItem = document.createElement("li");
+        newItem.classList.add("message__item");
+
+        newItem.innerHTML = `
+            <img src=${elem.imageUrl} alt="">
+            <p>${elem.value}</p>
+        `
+        htmlElement.appendChild(newItem);
+        newItem.addEventListener('click', function () {
+            if (elem.itemList) {
+                handleChatbotNextOptions(elem);
+            } else {
+                window.location.href = elem.redirectUrl;
+            }
+        })
+    })
+}
+
+function handleChatbotNextOptions(elem) {
+    const newList = elem.itemList;
+    renderChatbotTopics(newList);
+}
 
 function handleOpenChatbot() {
     chatbot.classList.remove("hidden");
@@ -10,12 +226,12 @@ function handleCloseChatbot() {
     chatbot.classList.add("hidden");
 }
 
-openChatbotBtn.addEventListener('click', function(event) {
+openChatbotBtn.addEventListener('click', function (event) {
     event.preventDefault();
     handleOpenChatbot();
 })
 
-closeChatbotBtn.addEventListener('click', function(event) {
+closeChatbotBtn.addEventListener('click', function (event) {
     event.preventDefault();
     handleCloseChatbot();
 })
