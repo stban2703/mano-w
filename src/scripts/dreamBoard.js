f6604abd091ea132cfe097a926e0f63c8b878f6a
stban2703 <s.t.ban1401@gmail.com>
2021-04-16 15:02:30 -0500

    Added close and open dreamBoard form

--- src/scripts/dreamBoard.js
+++ src/scripts/dreamBoard.js
@@ -1,1 +1,12 @@
+const dreamBoardAddModal = document.querySelector(".dreamBoard__addModal");
 const dreamBoardAddBtn = document.querySelector(".dreamBoard__addBtn");
+const dreamBoardCloseModalBtn = document.querySelector(".dreamBoard__addClose");
+
+
+dreamBoardAddBtn.addEventListener('click', () => {
+    dreamBoardAddModal.classList.remove("hidden");
+})
+
+dreamBoardCloseModalBtn.addEventListener('click', () => {
+    dreamBoardAddModal.classList.add("hidden");
+})
