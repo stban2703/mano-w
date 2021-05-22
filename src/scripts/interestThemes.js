afd84a9f8793d8f3f0fb5036261a6f1d8531458b
stban2703 <s.t.ban1401@gmail.com>
2021-05-22 01:10:03 -0500

    Handle interestThemeNext btn

--- src/scripts/interestThemes.js
+++ src/scripts/interestThemes.js
@@ -1,45 +1,55 @@
 const interestThemesSection = document.querySelector(".interestThemes__content");
-
+const interestThemesNextBtn = document.querySelector(".interestThemes__next");
 let interestThemesList = [
     {
         title: "Emprendimiento",
         value: "emprendimiento",
-        url: "emprendimiento"
+        url: "emprendimiento",
+        isSelected: false
     },
     {
         title: "Motivación",
         value: "motivacion",
-        url: "motivacion"
+        url: "motivacion",
+        isSelected: false
     },
     {
         title: "Emocional",
         value: "emocional",
-        url: "emocional"
+        url: "emocional",
+        isSelected: false
     },
     {
         title: "Ahorro",
         value: "ahorro",
-        url: "ahorro"
+        url: "ahorro",
+        isSelected: false
     },
     {
         title: "Inversiones",
         value: "inversiones",
-        url: "inversiones"
+        url: "inversiones",
+        isSelected: false
     },
     {
         title: "Créditos",
         value: "creditos",
-        url: "creditos"
+        url: "creditos",
+        isSelected: false
     }
 ]
 
 renderInterestThemes(interestThemesList);
+handleInterestThemeNext();
 
 function renderInterestThemes(list) {
     interestThemesSection.innerHTML = "";
     list.forEach(elem => {
         const interestThemeItem = document.createElement("div");
         interestThemeItem.classList.add("interestThemes__content__item");
+        if (elem.isSelected) {
+            interestThemeItem.classList.add("interestThemes__content__item--selected");
+        }
         interestThemeItem.innerHTML =
             `
             <div class="interestThemes__content__item__graphic">
@@ -47,17 +57,27 @@ function renderInterestThemes(list) {
             </div>
             <p>${elem.title}</p>
             `
-        handleSelectInterestTheme(interestThemeItem);
+        handleSelectInterestTheme(interestThemeItem, elem);
         interestThemesSection.appendChild(interestThemeItem);
     })
 }
 
-function handleSelectInterestTheme(elem) {
+function handleSelectInterestTheme(elem, obj) {
     elem.addEventListener("click", () => {
-        if (elem.classList.contains("interestThemes__content__item--selected")) {
-            elem.classList.remove("interestThemes__content__item--selected")
-        } else {
-            elem.classList.add("interestThemes__content__item--selected")
-        }
+        obj.isSelected = !obj.isSelected;
+        renderInterestThemes(interestThemesList);
+    })
+}
+
+function handleInterestThemeNext() {
+    interestThemesNextBtn.addEventListener("click", () => {
+        let urlFilters = "";
+        interestThemesList.forEach((elem, i) => {
+            if (elem.isSelected) {
+                urlFilters += elem.value + "-";
+            }
+        })
+        urlFilters = urlFilters.substring(0, urlFilters.length - 1);
+        window.location = `events.html?${urlFilters}`;
     })
 }
